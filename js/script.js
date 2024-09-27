c24ea397c10ab31642da7ae3b611e33d8d637099
raneem0987 <raneemsameh@std.mans.edu.eg>
2024-09-27 19:50:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,38 @@ var stickyPosition = navbarElement.offsetTop;
 window.addEventListener("scroll",function(e){
 
     if(window.scrollY >= navbarElement.offsetTop){
-        navbarElement.classList.add("sticky");
+        if(mobileNavElement.classList.contains("hide")){
+
+            navbarElement.classList.add("sticky");
+        }else
+        {
+            navbarElement.classList.remove("sticky");
+        }
     }else
     {
         navbarElement.classList.remove("sticky");
     }
+})
+
+     /////mobile navbar/////
+
+var menuvarbar = document.querySelector(".menubar-icon");
+
+var closeMenuElement = document.querySelector(".close-menu");
+
+var mobileNavElement = document.querySelector(".mobile-nav");
+
+menuvarbar.addEventListener("click", function (e) {
+
+    mobileNavElement.classList.remove("hide");
+    navbarElement.classList.remove("sticky");
+    mobileNavElement.classList.remove("animate-fadeOutRight");
+    mobileNavElement.classList.add("animate-fadeInRight");
+
+})
+
+closeMenuElement.addEventListener("click", function(){
+    mobileNavElement.classList.add("hide");
+    mobileNavElement.classList.remove("animate-fadeInRight");
+    mobileNavElement.classList.add("animate-fadeOutRight");
 })
